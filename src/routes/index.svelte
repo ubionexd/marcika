dec67c81ef29f60298f767cc43aae3411710692f
UbiOne <70473378+ubionexd@users.noreply.github.com>
2022-05-30 19:04:46 +0000

    Initial commit

---
+++ src/routes/index.svelte
@@ -0,0 +1,26 @@
+<script context="module">
+	export const prerender = true;
+</script>
+
+<style>
+	:global(body) {
+		background-color: #0f172a;
+	}
+
+</style>
+
+<svelte:head>
+	<title>Sveltest</title>
+	<meta name="description" content="Svelte test app" />
+</svelte:head>
+
+<section>
+	<div class="text-center text-xl font-bold select-none flex justify-center items-center h-screen">
+		<div>
+			<h1 class="text-[#ff3e00]">SvelteKit</h1>
+			<h1 class="text-[#38bdf8]">with TailwindCSS</h1>
+			<p class="text-white mt-10 text-sm">Compiled, with no Virtual-DOM and Server-Side rendering.</p>
+			<a class="text-gray-400 mt-1 text-sm hover:underline underline-offset-2" href="https://github.com/ubionexd/sveltest">Feel free to use it on your project!</a>
+		</div>
+	</div>
+</section>
