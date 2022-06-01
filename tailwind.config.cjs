9295ef91a6f79df0da3aeeabd843926b0a12d1dd
UbiOne <70473378+ubionexd@users.noreply.github.com>
2022-06-01 08:32:05 +0200

    update font lib

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,7 +1,11 @@
 module.exports = {
   content: ['./src/**/*.{html,js,svelte,ts}'],
   theme: {
-    extend: {},
+    extend: {
+       fontFamily {
+          'poppins': ['Poppins 500', 'sans-serif'];
+      },
+    },
   },
   plugins: [],
 }
