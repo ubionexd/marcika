fe2e188d6054bbe5262f52e303efeb2bbeb12883
UbiOne <70473378+ubionexd@users.noreply.github.com>
2022-06-01 08:33:18 +0200

    update title's font

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -14,4 +14,4 @@
 	<meta name="description" content="Marcika Szentély Rewrite" />
 </svelte:head>
 
-<h1 class="text-center text-[15vw] font-semibold select-none flex justify-center items-center h-screen">M A R C I K A</h1>
+<h1 class="text-center text-[15vw] font-semibold select-none flex justify-center items-center h-screen font-poppins">M A R C I K A</h1>
